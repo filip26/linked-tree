12fb869cf8c81e84f1ef9c9273f5cdceaea9c5cd
Filip <filip26@gmail.com>
2024-09-03 00:16:28 +0200

    Minor improvements

--- java/src/main/java/com/apicatalog/linkedtree/traversal/DepthFirstSearch.java
+++ java/src/main/java/com/apicatalog/linkedtree/traversal/DepthFirstSearch.java
@@ -23,7 +23,29 @@ public class DepthFirstSearch {
                 && container.asFragment().terms().contains(term)) {
             consumer.accept(container.asFragment().property(term));
         }
+    }
 
+    public static LinkedContainer findFirstPostOrder(String term, LinkedContainer container) {
+
+        for (final LinkedNode node : container.nodes()) {
+            if (node.isContainer()) {
+                final LinkedContainer found = findFirstPostOrder(term, node.asContainer());
+                if (found != null) {
+                    return found;
+                }
+            }
+            if (node.isFragment()
+                    && node.asFragment().terms().contains(term)) {
+                return node.asFragment().property(term);
+            }
+        }
+
+        if (container.isFragment()
+                && container.asFragment().terms().contains(term)) {
+            return container.asFragment().property(term);
+        }
+
+        return null;
     }
 
 }
